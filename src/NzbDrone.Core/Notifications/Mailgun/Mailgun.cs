Sonarr/Sonarr

Add missing On Delete Notifications to Mailgun notifications

src/NzbDrone.Core/Notifications/Mailgun/Mailgun.cs
@@ -29,6 +29,20 @@ namespace NzbDrone.Core.Notifications.Mailgun
             _proxy.SendNotification(EPISODE_DOWNLOADED_TITLE, downloadMessage.Message, Settings);
         }
 
+        public override void OnEpisodeFileDelete(EpisodeDeleteMessage deleteMessage)
+        {
+            var body = $"{deleteMessage.Message} deleted.";
+
+            _proxy.SendNotification(EPISODE_DELETED_TITLE, body, Settings);
+        }
+
+        public override void OnSeriesDelete(SeriesDeleteMessage deleteMessage)
+        {
+            var body = $"{deleteMessage.Message}";
+
+            _proxy.SendNotification(SERIES_DELETED_TITLE, body, Settings);
+        }
+
         public override void OnHealthIssue(HealthCheck.HealthCheck healthCheckMessage)
         {
             _proxy.SendNotification(HEALTH_ISSUE_TITLE, healthCheckMessage.Message, Settings);

ServarrAdmin/Radarr

(cherry picked from commit a824fa44d21328d9c8bbb1ccfd6d247d4ff5c888)

src/NzbDrone.Core/Notifications/Mailgun/Mailgun.cs
@@ -9,7 +9,7 @@ namespace NzbDrone.Core.Notifications.Mailgun
     {
         private readonly IMailgunProxy _proxy;
         private readonly Logger _logger;
-        
+
         public MailGun(IMailgunProxy proxy, Logger logger)
         {
             _proxy = proxy;
@@ -27,6 +27,20 @@ namespace NzbDrone.Core.Notifications.Mailgun
         public override void OnDownload(DownloadMessage downloadMessage)
         {
             _proxy.SendNotification(MOVIE_GRABBED_TITLE, downloadMessage.Message, Settings);
+        }
+
+        public override void OnMovieFileDelete(MovieFileDeleteMessage deleteMessage)
+        {
+            var body = $"{deleteMessage.Message} deleted.";
+
+            _proxy.SendNotification(MOVIE_FILE_DELETED_TITLE, body, Settings);
+        }
+
+        public override void OnMovieDelete(MovieDeleteMessage deleteMessage)
+        {
+            var body = $"{deleteMessage.Message}";
+
+            _proxy.SendNotification(MOVIE_DELETED_TITLE, body, Settings);
         }
 
         public override void OnHealthIssue(HealthCheck.HealthCheck healthCheckMessage)
@@ -51,7 +65,7 @@ namespace NzbDrone.Core.Notifications.Mailgun
                 _logger.Error(ex, "Unable to send test message though Mailgun.");
                 failures.Add(new ValidationFailure("", "Unable to send test message though Mailgun."));
             }
-            
+
             return new ValidationResult(failures);
         }
     }