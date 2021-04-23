Sonarr/Sonarr

New: Allow user to choose whether delay profile should apply to release of the highest enabled quality

src/NzbDrone.Core.Test/DecisionEngineTests/RssSync/DelaySpecificationFixture.cs
@@ -124,8 +124,19 @@ namespace NzbDrone.Core.Test.DecisionEngineTests.RssSync
         }
 
         [Test]
-        public void should_be_true_when_quality_and_language_is_last_allowed_in_profile()
+        public void should_be_false_when_quality_and_language_is_last_allowed_in_profile_and_bypass_disabled()
         {
+            _remoteEpisode.ParsedEpisodeInfo.Quality = new QualityModel(Quality.Bluray720p);
+            _remoteEpisode.ParsedEpisodeInfo.Language = Language.French;
+
+            Subject.IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_be_true_when_quality_and_language_is_last_allowed_in_profile_and_bypass_enabled()
+        {
+            _delayProfile.BypassIfHighestQuality = true;
+
             _remoteEpisode.ParsedEpisodeInfo.Quality = new QualityModel(Quality.Bluray720p);
             _remoteEpisode.ParsedEpisodeInfo.Language = Language.French;
 

ServarrAdmin/Radarr

(cherry picked from commit d668e923af83ab7f1589d947fec9f40a3919e0b8)

src/NzbDrone.Core.Test/DecisionEngineTests/RssSync/DelaySpecificationFixture.cs
@@ -104,8 +104,18 @@ namespace NzbDrone.Core.Test.DecisionEngineTests.RssSync
         }
 
         [Test]
-        public void should_be_true_when_quality_is_last_allowed_in_profile()
+        public void should_be_true_when_quality_is_last_allowed_in_profile_and_bypass_disabled()
         {
+            _remoteMovie.ParsedMovieInfo.Quality = new QualityModel(Quality.Bluray720p);
+
+            Subject.IsSatisfiedBy(_remoteMovie, null).Accepted.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_be_true_when_quality_is_last_allowed_in_profile_and_bypass_enabled()
+        {
+            _delayProfile.BypassIfHighestQuality = true;
+
             _remoteMovie.ParsedMovieInfo.Quality = new QualityModel(Quality.Bluray720p);
 
             Subject.IsSatisfiedBy(_remoteMovie, null).Accepted.Should().BeTrue();